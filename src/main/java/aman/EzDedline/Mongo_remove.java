2afdda712193d01a5d2e8561f789dd0d1ff58c16
Aman281201 <brofistaman@gmail.com>
2021-05-16 01:46:01 +0530

    added more features in remove function

--- src/main/java/aman/EzDedline/Mongo_remove.java
+++ src/main/java/aman/EzDedline/Mongo_remove.java
@@ -3,10 +3,14 @@ package aman.EzDedline;
 import com.mongodb.BasicDBObject;
 import com.mongodb.client.*;
 import org.bson.Document;
+import java.util.Date;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class Mongo_remove {
 
-    public static void main(String name , String course, Boolean key)
+    public static void main(String name = "" , String course, int key)
     {
         final String uri = "mongodb+srv://amank:aman@cluster0.7wsis.mongodb.net/DB?retryWrites=true&w=majority";
         MongoClient mongoClient = MongoClients.create(uri);
@@ -14,7 +18,7 @@ public class Mongo_remove {
         MongoDatabase DB = MongoClients.create().getDatabase("DB");
         MongoCollection<Document> collection = DB.getCollection("collection1");
 
-        if(!key) {
+        if(key == 1) {
 
             FindIterable<Document> it = collection.find();
             MongoCursor<Document> cur = it.iterator();
@@ -24,8 +28,41 @@ public class Mongo_remove {
 
             collection.deleteOne(del);
         }
+        else if(key == 2)
+        {
+            FindIterable<Document> it = collection.find();
+            MongoCursor<Document> cur = it.iterator();
 
-        else
+            while(cur.hasNext())
+            {
+                String dat_tm , n, c;
+                dat_tm = (String) cur.next().get("dat_tm");
+                n = (String) cur.next().get("name");
+                c = (String) cur.next().get("course");
+
+                SimpleDateFormat sdf = new SimpleDateFormat("hh:mm dd/MM/yyyy");
+                Date date1; // current date
+                Date date2 = null;
+                try {
+                    date2 = sdf.parse(dat_tm);
+                }
+                catch (ParseException e){
+                    e.printStackTrace();
+                }
+                if(date1.after(date2))
+                {
+                    Document doc = new Document();
+                    doc.append("name", n).append("course", c);
+
+                    collection.deleteOne(doc);
+                }
+
+
+
+            }
+        }
+
+        else if(key == 3)
         {
             BasicDBObject del_all = new BasicDBObject();
             collection.deleteMany(del_all);
--- src/main/java/aman/EzDedline/commands/Remove_dl.java
+++ src/main/java/aman/EzDedline/commands/Remove_dl.java
@@ -18,26 +18,48 @@ public class Remove_dl extends ListenerAdapter {
 
             if(args.length >= 2) {
                 String name = args[1], course = args[2];
-                Boolean y = false;
+                int y = 0;
                 try {
-                    Mongo_remove mongo_remove = new Mongo_remove();
-                    mongo_remove.main(name, course, y);
+                    if(args.length > 2) {
+                        y = 1;
+                        Mongo_remove mongo_remove = new Mongo_remove();
+
+                        mongo_remove.main(name, course, y);
+                        }
+                    else {
+                        if (args[1] == "completed") {
+                            y = 2;}
+                        else if(args[1] == "all"){
+                            y = 3;
+                        }
+                            Mongo_remove mongo_remove = new Mongo_remove();
+                            mongo_remove.main("abc", "xyz", y);
 
 
-                    EmbedBuilder success = new EmbedBuilder();
-                    success.setColor(Color.decode("#80ff80"));
-                    success.setTitle("Successful");
-                    success.setDescription("Successfully removed deadline " + name + " of course " + course );
-                    success.setFooter(event.getMember().getUser().getAsTag(), event.getMember().getUser().getAvatarUrl());
 
-                    event.getChannel().sendMessage(success.build()).queue();
+                    }
+                    if(y!=0){
 
-                }
+                        EmbedBuilder success = new EmbedBuilder();
+                        success.setColor(Color.decode("#80ff80"));
+                        success.setTitle("Successful");
+                        if(y == 1)
+                            success.setDescription("Successfully removed deadline " + name + " of course " + course);
+                        else if(y==2 || y == 3)
+                            success.setDescription("Successfully removed deadlines ");
+
+                        success.setFooter(event.getMember().getUser().getAsTag(), event.getMember().getUser().getAvatarUrl());
+
+                        event.getChannel().sendMessage(success.build()).queue();
+                    }}
+
+
+
                 catch (IllegalArgumentException e)
                 {
                     EmbedBuilder err = new EmbedBuilder();
                     err.setTitle("Information entered is either wrong or in wrong format");
-                    err.setDescription("Enter in following format {remove <Name> <course> or {remove all");
+                    err.setDescription("Enter in following format {remove <Name> <course> or {remove completed or {remove all");
                     err.setColor(Color.decode("#ff4d4d"));
                     err.setFooter(event.getMember().getUser().getAsTag(), event.getMember().getUser().getAvatarUrl());
 
@@ -54,6 +76,7 @@ public class Remove_dl extends ListenerAdapter {
                 remove.setTitle("\uD83D\uDC31\u200D\uD83D\uDC09Remove Deadline ");
                 remove.setDescription("Enter the following commands for precise results");
                 remove.addField("{remove <Name> <Course>", "to remove the deadline", false);
+                remove.addField("{remove completed", "removes all deadlines that are over", false);
                 remove.addField("{remove all", "to clear all the deadlines", false);
                 remove.setColor(Color.decode("#3366ff"));
                 remove.setFooter(event.getMember().getUser().getAsTag(), event.getMember().getUser().getAvatarUrl());
